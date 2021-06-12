e03f8a5ad1651faf6c87778a89cd4c552a481d54
tritonJS826 <triton.68@mail.ru>
2021-06-12 10:23:43 +0300

    Add components from previous version [Animated span, Event listener, Input button, Input text, Text]

---
+++ frontend/src/Components/AnimatedSpan/AnimatedSpan.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import textAppearance from './textAppearance';
+
+interface IAnimatedSpan extends React.HTMLAttributes<HTMLSpanElement>{
+  text: string | null;
+}
+
+const AnimatedSpan: React.FC<IAnimatedSpan> = ({ text, ...props }) => {
+  return (
+    <span {...props}>
+      {textAppearance(text ?? '')}
+    </span>
+)
+};
+
+export default AnimatedSpan;
---
+++ frontend/src/Components/InputButton/InputButton.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import styles from'./InputButton.module.scss';
+
+interface IInputButton extends React.HTMLAttributes<HTMLInputElement>{
+}
+
+const InputText: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = React.forwardRef((props: IInputButton, ref: any) => {
+  return (
+    <input type="button" ref={ref} className={styles["input-text"]} {...props} />
+)
+});
+
+export default InputText;
