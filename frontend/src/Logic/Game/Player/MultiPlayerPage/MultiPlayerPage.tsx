231e6bbdec84442a64bbef944a3a6a9c46c19658
tritonJS826 <triton.68@mail.ru>
2021-07-15 21:31:13 +0300

    join the game
    Co-authored-by: NastyaSvich <NastyaSvich@users.noreply.github.com>

--- frontend/src/Logic/Game/Player/MultiPlayerPage/MultiPlayerPage.tsx
+++ frontend/src/Logic/Game/Player/MultiPlayerPage/MultiPlayerPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from 'Logic/Game/Player/MultiPlayerPage/MultiPlayerPage.module.scss';
 import InputButton from 'Components/InputButton/InputButton';
 import SelectWithLabel from 'Components/SelectWithLabel/SelectWithLabel';
@@ -6,12 +6,13 @@ import InputText from 'Components/InputText';
 import MultiplayerStore from 'Logic/Game/Player/MultiPlayerPage/MultiplayerStore';
 import multiText from 'Resources/MultiLangText/MultiplayerPage.json';
 import { multiLang } from 'App';
-import { useLocalStore } from 'mobx-react';
+import { observer, useLocalStore } from 'mobx-react';
 import { useHistory } from 'react-router';
 import AppRoutes from 'AppRoutes';
 import Image from 'Components/Image/Image';
 import BloodMoon from 'Resources/bloodMoon.jpg';
 import { Difficulty } from 'Model/Difficulty';
+import { Link } from 'react-router-dom';
 
 /*
  * MultiPlayer page
@@ -19,6 +20,10 @@ import { Difficulty } from 'Model/Difficulty';
 const MultiPlayer: React.FC = () => {
   const history = useHistory();
   const multiplayerStore = useLocalStore(() => new MultiplayerStore());
+
+  useEffect(() => {
+    multiplayerStore.loadData();
+  }, []);
 
   const testOptions = [{
     key: Difficulty.ALL,
@@ -33,6 +38,34 @@ const MultiPlayer: React.FC = () => {
     data: Difficulty.HARD,
     value: Difficulty.HARD,
   }];
+
+  const onChangeId = (e: React.FormEvent<HTMLInputElement>) => {
+    multiplayerStore.setFilter({
+      ...multiplayerStore.filter,
+      id: e.currentTarget.value,
+    });
+  };
+
+  const onChangeHostName = (e: React.FormEvent<HTMLInputElement>) => {
+    multiplayerStore.setFilter({
+      ...multiplayerStore.filter,
+      hostName: e.currentTarget.value,
+    });
+  };
+
+  const onChangeWorld = (e: React.FormEvent<HTMLInputElement>) => {
+    multiplayerStore.setFilter({
+      ...multiplayerStore.filter,
+      world: e.currentTarget.value,
+    });
+  };
+
+  const onChangeDifficulty = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    multiplayerStore.setFilter({
+      ...multiplayerStore.filter,
+      difficulty: e.currentTarget.value as Difficulty,
+    });
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -62,29 +95,56 @@ const MultiPlayer: React.FC = () => {
           value={multiplayerStore.filter.id}
           label={multiLang.text(multiText.multiplayerPage.filter.id)}
           style={{ marginRight: 20, marginTop: 10, width: 200 }}
+          onChange={onChangeId}
         />
 
         <InputText
-          value={multiplayerStore.filter.word}
+          value={multiplayerStore.filter.world}
           label={multiLang.text(multiText.multiplayerPage.filter.world)}
           style={{ marginRight: 20, marginTop: 10, width: 200 }}
+          onChange={onChangeWorld}
         />
 
         <InputText
           value={multiplayerStore.filter.hostName}
           label={multiLang.text(multiText.multiplayerPage.filter.hostName)}
           style={{ marginRight: 20, marginTop: 10, width: 200 }}
+          onChange={onChangeHostName}
         />
 
         <SelectWithLabel
           style={{ marginRight: 20, marginTop: 10, width: 200 }}
           label={multiLang.text(multiText.multiplayerPage.filter.difficulty)}
           options={testOptions}
+          onChange={onChangeDifficulty}
         />
       </div>
 
-      <div className={styles.table}>TABLE</div>
+      <div className={styles.table}>
+        <div className={styles['table-row']}>
+          <div className={styles.cell}>{multiLang.text(multiText.multiplayerPage.filter.id)}</div>
+          <div className={styles.cell}>{multiLang.text(multiText.multiplayerPage.filter.world)}</div>
+          <div className={styles.cell}>{multiLang.text(multiText.multiplayerPage.filter.hostName)}</div>
+          <div className={styles.cell}>{multiLang.text(multiText.multiplayerPage.filter.difficulty)}</div>
+          <div className={styles.cell}>{multiLang.text(multiText.multiplayerPage.table.action)}</div>
+        </div>
+
+        {multiplayerStore.tableDataWithFilter.map((rowData) => (
+          <div className={styles['table-row']} key={rowData.id}>
+            <div className={styles.cell}>{rowData.id}</div>
+            <div className={styles.cell}>{rowData.world}</div>
+            <div className={styles.cell}>{rowData.hostName}</div>
+            <div className={styles.cell}>{rowData.difficulty}</div>
+            <div className={styles.cell}>
+              <Link to={AppRoutes.preGame.toUrl()}>
+                {multiLang.text(multiText.multiplayerPage.join)}
+              </Link>
+            </div>
+
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-export default MultiPlayer;
+export default observer(MultiPlayer);
--- frontend/src/Logic/Game/Player/MultiPlayerPage/MultiplayerStore.ts
+++ frontend/src/Logic/Game/Player/MultiPlayerPage/MultiplayerStore.ts
@@ -1,24 +1,116 @@
-import { makeObservable, action } from 'mobx';
+import {
+  makeObservable, action, observable, computed,
+} from 'mobx';
 import { Difficulty } from 'Model/Difficulty';
 
+interface HostTableData {
+  id: string;
+  world: string;
+  hostName: string;
+  difficulty: string;
+}
 interface Filter {
-    id?: string;
-    word?: string;
-    hostName?: string;
-    difficulty?: Difficulty;
+  id?: string;
+  world?: string;
+  hostName?: string;
+  difficulty?: Difficulty;
 }
 
 class MultiplayerStore {
-     filter: Filter = {};
+  @observable
+  filter: Filter = {};
 
-     constructor() {
-       makeObservable(this);
-     }
+  @observable
+  tableData: HostTableData[] = [];
 
-    @action.bound
-     setFilter(filter: Filter) {
-       this.filter = filter;
-     }
+  constructor() {
+    makeObservable(this);
+  }
+
+  @action.bound
+  setFilter(filter: Filter) {
+    this.filter = filter;
+  }
+
+  @action.bound
+  async loadData(): Promise<void> {
+    this.tableData = tableData;
+  }
+
+  @computed
+  get tableDataWithFilter() {
+    return this.tableData
+      .filter((cellData) => {
+        const isIdMatch = this.filter.id ? cellData.id.includes(this.filter.id) : true;
+        const isWordMatch = this.filter.world ? cellData.world.includes(this.filter.world) : true;
+        const isHostNameMatch = this.filter.hostName ? cellData.hostName.includes(this.filter.hostName) : true;
+        const isDifficultyMatch = this.filter.difficulty ? cellData.difficulty.includes(this.filter.difficulty) : true;
+
+        return isIdMatch && isWordMatch && isHostNameMatch && isDifficultyMatch;
+      });
+  }
 }
 
 export default MultiplayerStore;
+
+const tableData = [{
+  id: '1',
+  world: 'world1',
+  hostName: 'hostName',
+  difficulty: 'easy',
+}, {
+  id: '2',
+  world: 'world2',
+  hostName: 'hostName2',
+  difficulty: 'hard',
+}, {
+  id: '3',
+  world: 'world1',
+  hostName: 'hostName',
+  difficulty: 'easy',
+}, {
+  id: '4',
+  world: 'world2',
+  hostName: 'hostName2',
+  difficulty: 'hard',
+}, {
+  id: '5',
+  world: 'world1',
+  hostName: 'hostName',
+  difficulty: 'easy',
+}, {
+  id: '6',
+  world: 'world2',
+  hostName: 'hostName2hostName2',
+  difficulty: 'hard',
+}, {
+  id: '7',
+  world: 'world1',
+  hostName: 'hostName',
+  difficulty: 'easy',
+}, {
+  id: '8',
+  world: 'world2',
+  hostName: 'hostName2',
+  difficulty: 'hard',
+}, {
+  id: '9',
+  world: 'world1',
+  hostName: 'hostName',
+  difficulty: 'easy',
+}, {
+  id: '10',
+  world: 'world2',
+  hostName: 'hostName2',
+  difficulty: 'hard',
+}, {
+  id: '11',
+  world: 'world1',
+  hostName: 'hostName',
+  difficulty: 'easy',
+}, {
+  id: '12',
+  world: 'world2',
+  hostName: 'hostName2',
+  difficulty: 'hard',
+}];
