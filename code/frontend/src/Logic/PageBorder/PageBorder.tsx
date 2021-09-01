712c8fa092a33a94f9dc5ec7d38b1b72eefa9d04
tritonJS826 <triton.68@mail.ru>
2021-09-01 19:25:35 +0300

    add page border

--- code/frontend/src/Logic/PageBorder/PageBorder.tsx
+++ code/frontend/src/Logic/PageBorder/PageBorder.tsx
@@ -1,6 +1,9 @@
 import Image from 'Components/Image/Image';
 import { PropsWithChildren } from 'react';
-import BloodMoon from 'Resources/bloodMoon.jpg';
+import BloodMoon7 from 'Resources/bloodBack7+.jpg';
+import BloodMoon13 from 'Resources/bloodBack13+.jpg';
+import BloodMoon14 from 'Resources/bloodBack14+.jpg';
+import BloodMoon15 from 'Resources/bloodBack15+.jpg';
 import styles from 'Logic/PageBorder/PageBorder.module.scss';
 
 interface PageBorderProps {
@@ -8,16 +11,24 @@ interface PageBorderProps {
 }
 
 const PageBorder: React.FC<PropsWithChildren<PageBorderProps>> = (props: PropsWithChildren<PageBorderProps>) => {
-  const t = 'test';
+  const images = [
+    BloodMoon7,
+    BloodMoon13,
+    BloodMoon14,
+    BloodMoon15,
+  ];
+
+  const getRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+
+    return images[randomIndex];
+  };
+
   return (
     <div className={styles.wrapper}>
       <Image
-        src={BloodMoon}
-        style={{
-          zIndex: -50,
-          position: 'fixed',
-          height: '100%',
-        }}
+        src={getRandomImage()}
+        className={styles['appear-from-dark']}
       />
       {props.children}
     </div>
